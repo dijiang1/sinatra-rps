98284aa203136d8a59bb004de62656bc2c4c80de
dijiang1 <djiang10@chicagobooth.edu>
2025-01-25 04:05:57 +0000

    V1

--- app.rb
+++ app.rb
@@ -1,9 +1,52 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'better_errors'
+require 'binding_of_caller'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+# Configuration for better_errors
+use BetterErrors::Middleware
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+# Method to determine the game outcome
+def determine_outcome(player_choice, computer_choice)
+  if player_choice == computer_choice
+    'tied!'
+  elsif (player_choice == 'rock' && computer_choice == 'scissors') ||
+        (player_choice == 'paper' && computer_choice == 'rock') ||
+        (player_choice == 'scissors' && computer_choice == 'paper')
+    'won!'
+  else
+    'lost!'
+  end
 end
+
+# Route for the homepage
+get '/' do
+  erb :homepage
+end
+
+# Route for playing rock
+get '/rock' do
+  @player_choice = 'rock'
+  @computer_choice = ['rock', 'paper', 'scissors'].sample
+  @outcome = determine_outcome(@player_choice, @computer_choice)
+  erb :rock
+end
+
+# Route for playing paper
+get '/paper' do
+  @player_choice = 'paper'
+  @computer_choice = ['rock', 'paper', 'scissors'].sample
+  @outcome = determine_outcome(@player_choice, @computer_choice)
+  erb :paper
+end
+
+# Route for playing scissors
+get '/scissors' do
+  @player_choice = 'scissors'
+  @computer_choice = ['rock', 'paper', 'scissors'].sample
+  @outcome = determine_outcome(@player_choice, @computer_choice)
+  erb :scissors
+end
+
